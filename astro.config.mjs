a0d010cc56c4e578cbffcf8acecd66e515147571
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-09-16 21:58:59 +0200

    Add NotebookLM podcast

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import rehypeKatex from "rehype-katex";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://feature-space.com',
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
